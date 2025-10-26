1a337508f129d32da45fdcc053535f8b71812b50
sergioarroyop <sergioarroyopay@gmail.com>
2025-10-26 22:35:19 +0100

    fix lib missing folder

---
+++ code/lib/mealie.py
@@ -0,0 +1,43 @@
+import logging
+import requests
+import json
+
+from config.settings import MEALIE_API_URL, MEALIE_TOKEN
+
+logger = logging.getLogger(__name__)
+
+def create_receipe(json_receipe: str) -> bool | None:
+    try:
+        mealie_api = MEALIE_API_URL + "/api/recipes/create/html-or-json"
+        
+        parsed_json = json.loads(json_receipe)
+        escaped_json = json.dumps(parsed_json, ensure_ascii=False)
+        
+        payload = {
+            "data": escaped_json,
+            "includeTags": True
+        }
+        headers = {
+            "Authorization": f"Bearer {MEALIE_TOKEN}",
+            "Content-Type": "application/json",
+            "User-Agent": "Mealie-Reciper"
+        }
+        
+        body = json.dumps(payload, ensure_ascii=False)
+        
+        resp = requests.post(
+            url=mealie_api,
+            data=body,
+            headers=headers
+        )
+        if resp.status_code != 201:
+            logger.error(f"🙁 Error creating Mealie receipe:\n{resp.json()}\n{json_receipe}")
+            return True
+        else:
+            logger.info(f"🎉 Mealie receipe was successfully created! {MEALIE_API_URL}/g/home/r/{resp.json()}")
+            return False
+    except Exception as e:
+        logger.error(f"❌ There was an issue creating mMeali receipe\n{e}")
+        raise
+    
+    
